e5ba3e630154abb0e93d87cbb52fd8513ff127b5
Sanidhya786 <kumarsanidhya02@gmail.com>
2024-09-30 15:20:39 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,35 +3,7 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    import java.util.Stack;
-
-public class Solution {
-    public boolean isValid(String s) {
-        // Stack to store opening brackets
-        Stack<Character> stack = new Stack<>();
-        
-        // Loop through each character in the string
-        for (char c : s.toCharArray()) {
-            // If it's an opening bracket, push it onto the stack
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            }
-            // If it's a closing bracket
-            else {
-                // If the stack is empty or the top of the stack doesn't match the corresponding opening bracket, return false
-                if (stack.isEmpty() || 
-                   (c == ')' && stack.pop() != '(') || 
-                   (c == '}' && stack.pop() != '{') || 
-                   (c == ']' && stack.pop() != '[')) {
-                    return false;
-                }
-            }
-        }
-        // If the stack is empty, the string is valid, otherwise, it's not
-        return stack.isEmpty();
-    }
-}
-
+    
 };
 
 module.exports = { isValid };
