dc4153e6d05afdc9ef3b359fcfe4028ecf00e6e2
Sanidhya786 <kumarsanidhya02@gmail.com>
2024-09-30 15:15:10 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,10 +2,8 @@ import java.util.Stack;
 
 public class Solution {
     public boolean isValid(String s) {
-        // Stack to store opening brackets
         Stack<Character> stack = new Stack<>();
         
-        // Loop through each character in the string
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
