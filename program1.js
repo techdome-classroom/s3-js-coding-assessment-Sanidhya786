80afdd51d2a7857a805d4dad0231f88a19e38547
Sanidhya786 <kumarsanidhya02@gmail.com>
2024-09-30 15:33:17 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,23 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
+const isValid = (s) => {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else if (stack.length === 0 || stack.pop() !== map[char]) {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
 };
 
-module.exports = { isValid };
-
-
+// Test the function
+console.log(isValid("()"));       // Output: true
+console.log(isValid("()[]{}"));   // Output: true
+console.log(isValid("(]"));       // Output: false
